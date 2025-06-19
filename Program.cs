3fce1b72827ea90855a17eaa13102f994fa22c39
MotiWolff <motiwolff@gmail.com>
2025-06-19 11:17:20 +0300

    Implement Bird and Penguin classes with Fly method functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Bird
+{
+    public virtual void Fly()
+    {
+        Console.WriteLine("I'm flying!");
+    }
+}
+
+public class Penguin : Bird
+{
+    public override void Fly()
+    {
+        Console.WriteLine("I cannot fly.");
+    }
+}
+
+
+class Program
+{
+    public static void MakeBirdFly(Bird b)
+    {
+        b.Fly();
+    }
+
+    public static void Main()
+    {
+        Bird sparrow = new Bird();
+        Penguin penguin = new Penguin();
+
+        MakeBirdFly(sparrow);
+        MakeBirdFly(penguin);
+    }
+}
